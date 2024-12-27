5ee370983fdfa937a3524c7fef3e85f3b062d1ae
Sheikh-hadi <96363461+Sheikh-hadi@users.noreply.github.com>
2024-12-27 17:43:15 +0500

    collectionFloral

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 "use client"
+import "locomotive-scroll/dist/locomotive-scroll.css";
 import ArtistPage from "./Pages/Artist"
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ContactUs from "./Pages/ContactUs/ContactUs";
 import AnnualLadyBug from "./Pages/AnnualLadyBug";
 import Timeline from "./Pages/Timeline";
-
+import Home from "./Pages/Home";
+import Slider from "./Slider";
+import Slideshow3d from "./components/Slideshow3d/Slidershow3d";
 // import Timeline from "./Pages/Timeline"
 const App = () => {
   return (
@@ -14,6 +17,9 @@ const App = () => {
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/annual-lady-bug" element={<AnnualLadyBug />} />
         <Route path="/timeline" element={<Timeline />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/collection" element={<Slider />} />
+        <Route path="/slideShow3d" element={<Slideshow3d />} />
       </Routes>
     </Router>
   )
